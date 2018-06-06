97ccb426b590140cbcfc91a6176defdb6e080d43
RadoslawWojdak <radoslawwojdak95@gmail.com>
2018-06-06 21:14:04 +0200

    NoteWindow New content:  -NoteWindow with Menu and Tags, Title and Note textBoxes.
    Others:
     -Adjusted MainWindow content to the NoteWindow(s)

--- MyDiary/MainWindow.xaml.cs
+++ MyDiary/MainWindow.xaml.cs
@@ -20,31 +20,63 @@ namespace MyDiary
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool _closed;
+
         public MainWindow()
         {
             InitializeComponent();
-            AdjustControlsParameters();
+
+            _closed = false;
+
+            AdjustControlsParameters(Width, Height);
         }
 
-        private void AdjustControlsParameters()
+        //at the beginning "ActualWidth" and "ActualHeight" is equal to 0
+        private void AdjustControlsParameters(double winWidth, double winHeight)
         {
-            menu.Width = ActualWidth;
+            menu.Width = winWidth;
+        }
+        
+        private void Close()
+        {
+            for (int i = 0; i < OwnedWindows.Count; i++)
+            {
+                Window win = OwnedWindows[i];
+                win.Close();
+            }
+
+            if (!_closed && OwnedWindows.Count == 0)
+            {
+                _closed = true;
+                Window win = this;
+                win.Close();
+            }
+        }
+
+        private void Close(System.ComponentModel.CancelEventArgs e)
+        {
+            if (!_closed)
+            {
+                _closed = true;
+                Close();
+                if (OwnedWindows.Count > 0)
+                {
+                    e.Cancel = true;
+                    _closed = false;
+                }
+            }
         }
 
         private void menuNewNote_Click(object sender, RoutedEventArgs e)
         {
-            ;
+            NoteWindow note = new NoteWindow();
+            note.Owner = this;
+            note.Show();
         }
 
         private void menuNewDiary_Click(object sender, RoutedEventArgs e)
         {
             ;
-        }
-
-        private void Close()
-        {
-            Window win = GetWindow(this);
-            win.Close();
         }
 
         private void menuExit_Click(object sender, RoutedEventArgs e)
@@ -59,22 +91,22 @@ namespace MyDiary
 
         private void menuVersion_Click(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("v.0.0.1", "MyDiary");
+            MessageBox.Show("v. pre-alpha", "MyDiary");
         }
 
         private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
         {
-            AdjustControlsParameters();
+            AdjustControlsParameters(ActualWidth, ActualHeight);
         }
 
         private void Window_StateChanged(object sender, EventArgs e)
         {
-            AdjustControlsParameters();
+            AdjustControlsParameters(ActualWidth, ActualHeight);
         }
 
-        private void Window_Closed(object sender, EventArgs e)
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            Close();
+            Close(e);
         }
     }
 }
---
+++ MyDiary/NoteWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyDiary
+{
+    /// <summary>
+    /// Interaction logic for NoteWindow.xaml
+    /// </summary>
+    public partial class NoteWindow : Window
+    {
+        private bool _isClosed;
+        public bool Closed { get { return _isClosed; } set { _isClosed = value; } }
+
+        public NoteWindow()
+        {
+            InitializeComponent();
+
+            _isClosed = false;
+            tagsTextBox.Text = "";
+            titleTextBox.Text = "";
+            noteTextBox.Text = "";
+
+            AdjustControlsParameters(Width, Height);
+        }
+
+        //at the beginning "ActualWidth" and "ActualHeight" is equal to 0
+        private void AdjustControlsParameters(double winWidth, double winHeight)
+        {
+            menu.Width = winWidth;
+
+            double textBoxLeft = Math.Max(tagsLabel.ActualWidth, titleLabel.ActualWidth);
+            double textBoxWidth = winWidth - textBoxLeft - 20;
+            
+            tagsTextBox.Width = textBoxWidth;
+            tagsTextBox.Margin = new Thickness(textBoxLeft, menu.Height + 2, 0, 0);
+
+            titleTextBox.Width = textBoxWidth;
+            titleTextBox.Margin = new Thickness(textBoxLeft, tagsTextBox.Margin.Top + tagsTextBox.ActualHeight, 0, 0);
+
+            noteTextBox.Width = winWidth - 24;
+            noteTextBox.Height = winHeight - menu.Height - titleTextBox.Height - 42;
+        }
+
+        public void Close()
+        {
+            MessageBoxResult mbResult = MessageBox.Show("Do you want to save the changes?", "Save note", MessageBoxButton.YesNoCancel);
+
+            if (mbResult != MessageBoxResult.Cancel)
+            {
+                Window win = this;
+                Closed = true;
+                win.Close();
+            }
+        }
+
+        public void Close(System.ComponentModel.CancelEventArgs e)
+        {
+            if (!Closed)
+            {
+                MessageBoxResult mbResult = MessageBox.Show("Do you want to save the changes?", "Save note", MessageBoxButton.YesNoCancel);
+
+                if (mbResult == MessageBoxResult.Cancel)
+                    e.Cancel = true;
+                else
+                    Closed = true;
+            }
+        }
+
+        private void menuSave_Click(object sender, RoutedEventArgs e)
+        {
+            ;
+        }
+
+        private void menuLoad_Click(object sender, RoutedEventArgs e)
+        {
+            ;
+        }
+
+        private void menuClose_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Close(e);
+        }
+
+        private void Window_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            AdjustControlsParameters(ActualWidth, ActualHeight);
+        }
+
+        private void Window_StateChanged(object sender, EventArgs e)
+        {
+            AdjustControlsParameters(ActualWidth, ActualHeight);
+        }
+    }
+}
